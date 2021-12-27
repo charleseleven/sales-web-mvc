e5dc958d47ab99b27fbbdf56d4d5f0bae65c6c61
Charles Pires Rodrigues <cpiresrodrigues81@gmail.com>
2021-12-27 16:44:43 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,13 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Salles Web App from C# Course";
+            ViewData["Developer"] = "Charles II";
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
